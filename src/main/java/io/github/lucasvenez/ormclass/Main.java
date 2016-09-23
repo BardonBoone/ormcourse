c430bedd835c152219e024efb62922fa0665973c
a1502778 <a1502778@LAB209M15.lab209.campus.ifsp>
2016-09-22 22:23:53 -0300

    Estanciado classes Country, State, City, Person Individual

--- src/main/java/io/github/lucasvenez/ormclass/Main.java
+++ src/main/java/io/github/lucasvenez/ormclass/Main.java
@@ -3,18 +3,38 @@ package io.github.lucasvenez.ormclass;
 import java.util.Date;
 
 import io.github.lucasvenez.ormclass.dao.OrderDAO;
+import io.github.lucasvenez.ormclass.model.City;
+import io.github.lucasvenez.ormclass.model.Country;
+import io.github.lucasvenez.ormclass.model.Individual;
 import io.github.lucasvenez.ormclass.model.Order;
 import io.github.lucasvenez.ormclass.model.Person;
+import io.github.lucasvenez.ormclass.model.Product;
+import io.github.lucasvenez.ormclass.model.State;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
-		Person person = new Person();
+		Country country = new Country("Brasil");
 		
-		person.getBirthdate();
+		State state = new State("São Paulo", country);
+		
+		City city = new City("Caragua", state);
+		
+		
+		Person person = new Individual();
+		
+		person.getFullname();
+		
+        person.getBirthdate();
 		
 		person.getCity();
+		
+		
+		Product product = new Product();
+		
+		
+		
 		
 		Order order = new Order();
 		
